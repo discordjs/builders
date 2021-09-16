a809dc4e7c2518a202cf3b4c9142eff250fdaaee
suneettipirneni <suneettipirneni@icloud.com>
2021-09-16 15:27:27 -0400

    docs: document

--- src/interactions/slashCommands/mixins/CommandChannelOptionBase.ts
+++ src/interactions/slashCommands/mixins/CommandChannelOptionBase.ts
@@ -11,16 +11,25 @@ export abstract class ApplicationCommandOptionWithChannelTypesBase
 {
 	public channelTypes?: ChannelType[];
 
+	/**
+	 * Adds a channel type to this option
+	 * @param channelType The type of channel to allow
+	 */
 	public addChannelType(channelType: ChannelType) {
 		this.channelTypes ??= [];
 
 		ow(channelType, `channel type`, channelTypePredicate);
 		this.channelTypes.push(channelType);
+
 		return this;
 	}
 
-	public addChannelTypes(newChannelTypes: ChannelType[]) {
-		newChannelTypes.forEach((channelType) => this.addChannelType(channelType));
+	/**
+	 * Adds channel types to this option
+	 * @param channelTypes The channel types to add
+	 */
+	public addChannelTypes(channelTypes: ChannelType[]) {
+		channelTypes.forEach((channelType) => this.addChannelType(channelType));
 		return this;
 	}
 
