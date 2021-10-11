f6c187e0ad6ebe03e65186ece3e95cb1db5aeb50
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2021-10-11 07:59:29 -0400

    feat: add support for channel types on channel options (#41)
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>

---
+++ src/interactions/slashCommands/mixins/CommandChannelOptionBase.ts
@@ -0,0 +1,55 @@
+import { ChannelType } from 'discord-api-types/v9';
+import ow from 'ow';
+import type { ToAPIApplicationCommandOptions } from '../../..';
+import { SlashCommandOptionBase } from './CommandOptionBase';
+
+// Only allow valid channel types to be used. (This can't be dynamic because const enums are erased at runtime)
+const allowedChannelTypes = [
+	ChannelType.GuildCategory,
+	ChannelType.GuildNews,
+	ChannelType.GuildNewsThread,
+	ChannelType.GuildStore,
+	ChannelType.GuildStageVoice,
+	ChannelType.GuildText,
+	ChannelType.GuildVoice,
+	ChannelType.GuildPublicThread,
+	ChannelType.GuildPrivateThread,
+];
+
+const channelTypePredicate = ow.number.oneOf(allowedChannelTypes);
+
+export abstract class ApplicationCommandOptionWithChannelTypesBase
+	extends SlashCommandOptionBase
+	implements ToAPIApplicationCommandOptions
+{
+	public channelTypes?: ChannelType[];
+
+	/**
+	 * Adds a channel type to this option
+	 * @param channelType The type of channel to allow
+	 */
+	public addChannelType(channelType: Exclude<ChannelType, ChannelType.DM | ChannelType.GroupDM>) {
+		this.channelTypes ??= [];
+
+		ow(channelType, 'channel type', channelTypePredicate);
+		this.channelTypes.push(channelType);
+
+		return this;
+	}
+
+	/**
+	 * Adds channel types to this option
+	 * @param channelTypes The channel types to add
+	 */
+	public addChannelTypes(channelTypes: Exclude<ChannelType, ChannelType.DM | ChannelType.GroupDM>[]) {
+		channelTypes.forEach((channelType) => this.addChannelType(channelType));
+		return this;
+	}
+
+	public override toJSON() {
+		return {
+			...super.toJSON(),
+			channel_types: this.channelTypes,
+		};
+	}
+}
