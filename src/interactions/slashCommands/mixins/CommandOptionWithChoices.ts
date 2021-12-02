05b07a7e88848188c27d7380d9f948cba25ef778
Khasms <36800359+Khasms@users.noreply.github.com>
2021-12-02 01:20:15 -0800

    feat(SlashCommandBuilder): add autocomplete (#53)

--- src/interactions/slashCommands/mixins/CommandOptionWithChoices.ts
+++ src/interactions/slashCommands/mixins/CommandOptionWithChoices.ts
@@ -7,12 +7,14 @@ import { SlashCommandOptionBase } from './CommandOptionBase';
 const stringPredicate = z.string().min(1).max(100);
 const numberPredicate = z.number().gt(-Infinity).lt(Infinity);
 const choicesPredicate = z.tuple([stringPredicate, z.union([stringPredicate, numberPredicate])]).array();
+const booleanPredicate = z.boolean();
 
 export abstract class ApplicationCommandOptionWithChoicesBase<T extends string | number>
 	extends SlashCommandOptionBase
 	implements ToAPIApplicationCommandOptions
 {
 	public choices?: APIApplicationCommandOptionChoice[];
+	public readonly autocomplete?: boolean;
 
 	/**
 	 * Adds a choice for this option
@@ -20,7 +22,11 @@ export abstract class ApplicationCommandOptionWithChoicesBase<T extends string |
 	 * @param name The name of the choice
 	 * @param value The value of the choice
 	 */
-	public addChoice(name: string, value: T) {
+	public addChoice(name: string, value: T): Omit<this, 'setAutocomplete'> {
+		if (this.autocomplete) {
+			throw new RangeError('Autocomplete and choices are mutually exclusive to each other.');
+		}
+
 		this.choices ??= [];
 
 		validateMaxChoicesLength(this.choices);
@@ -42,17 +48,47 @@ export abstract class ApplicationCommandOptionWithChoicesBase<T extends string |
 	 *
 	 * @param choices The choices to add
 	 */
-	public addChoices(choices: [name: string, value: T][]) {
+	public addChoices(choices: [name: string, value: T][]): Omit<this, 'setAutocomplete'> {
+		if (this.autocomplete) {
+			throw new RangeError('Autocomplete and choices are mutually exclusive to each other.');
+		}
+
 		choicesPredicate.parse(choices);
 
 		for (const [label, value] of choices) this.addChoice(label, value);
 		return this;
 	}
 
+	/**
+	 * Marks the option as autocompletable
+	 * @param autocomplete If this option should be autocompletable
+	 */
+	public setAutocomplete<U extends boolean>(
+		autocomplete: U,
+	): U extends true
+		? Omit<this, 'addChoice' | 'addChoices'>
+		: this & Pick<ApplicationCommandOptionWithChoicesBase<T>, 'addChoice' | 'addChoices'> {
+		// Assert that you actually passed a boolean
+		booleanPredicate.parse(autocomplete);
+
+		if (autocomplete && Array.isArray(this.choices) && this.choices.length > 0) {
+			throw new RangeError('Autocomplete and choices are mutually exclusive to each other.');
+		}
+
+		Reflect.set(this, 'autocomplete', autocomplete);
+
+		return this;
+	}
+
 	public override toJSON() {
+		if (this.autocomplete && Array.isArray(this.choices) && this.choices.length > 0) {
+			throw new RangeError('Autocomplete and choices are mutually exclusive to each other.');
+		}
+
 		return {
 			...super.toJSON(),
 			choices: this.choices,
+			autocomplete: this.autocomplete,
 		};
 	}
 }
